1fc999801cfd3080d9f9f082f907fd4a49b22d04
Rohan Krishnamurthy <rohan.krishnamurthy@zf.com>
2025-08-21 09:31:47 +0200

    Update setup_ort_docker.sh

--- setup_ort_docker.sh
+++ setup_ort_docker.sh
@@ -1,2 +1,2 @@
-===> Running ORT scan...
-./run_scan_working.sh: line 74: JAVA_TOOL_OPTIONS_VALUE: unbound variable
+# Used for all JVM-based ORT steps (scan/advise/evaluate/report)
+JAVA_TOOL_OPTIONS_VALUE="-Djavax.net.ssl.trustStore=/ort/data/custom-cacerts.jks -Djavax.net.ssl.trustStorePassword=$TRUST_STORE_PASSWORD"
