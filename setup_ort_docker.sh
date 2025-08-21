dd3e83defbbfe9cb08386748b3d3a5d828ddf406
Rohan Krishnamurthy <rohan.krishnamurthy@zf.com>
2025-08-21 09:27:39 +0200

    Update setup_ort_docker.sh

--- setup_ort_docker.sh
+++ setup_ort_docker.sh
@@ -1,11 +1,2 @@
-docker run --rm -u "$(id -u):$(id -g)" \
-  -v "$PROJECT_DIR":/project \
-  -v "$OUTPUT_DIR":/ort/data \
-  -v "$CONFIG_DIR":/home/ort/.ort/config \
-  -e JAVA_TOOL_OPTIONS="$JAVA_TOOL_OPTIONS_VALUE" \
-  "$ORT_IMAGE" scan \
-  --ort-file "/ort/data/analyzer-result/$(basename "$ANALYZE_RESULT")" \
-  --package-types PROJECT \
-  --project-scanners ScanCode \
-  --skip-excluded \
-  -o /ort/data/scanner-result
+===> Running ORT scan...
+./run_scan_working.sh: line 74: JAVA_TOOL_OPTIONS_VALUE: unbound variable
